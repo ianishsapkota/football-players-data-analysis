a98588c4e8b31c6e1f41bec58771d49245bce79c
Anish Sapkota <anishsapkota926@gmail.com>
2025-11-27 21:17:51 +0545

    most baluable play for the vlub in terms of assist is added

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("attacking.csv")
 
+
+#top 10 assists
 top10_assists = df.sort_values(by='assists', ascending=False).head(10)
 print(top10_assists)
 
+#top 10 dribbles
 top10_dribbles = df.sort_values(by='dribbles',ascending=False).head(10)
 print(top10_dribbles)
 
-
+#playrs position distribution
 position_counts = df['position'].value_counts()
 plt.pie(position_counts,labels = position_counts.index)
 plt.title("Distribution of Players Position")
 
 
+
+#assist per club
 assists_per_club = df.groupby('club')['assists'].sum()
 
 plt.bar(assists_per_club.index, assists_per_club.values)
@@ -24,3 +29,6 @@ plt.title("Assists per Club (Sorted)")
 plt.xlabel("Club")
 plt.ylabel("Assists")
 plt.show()
+
+#most valuable player for the club in terms of assist
+df.loc[df.groupby('club')['assists'].idxmax(), ['club', 'player_name', 'assists']]
