4132028ba727a47f9fb9ccb25b802e58f0c913bc
Anish Sapkota <anishsapkota926@gmail.com>
2025-11-27 20:48:15 +0545

    players with top 10 dribbles are added

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ df = pd.read_csv("attacking.csv")
 
 top10_assists = df.sort_values(by='assists', ascending=False).head(10)
 print(top10_assists)
+
+top10_dribbles = df.sort_values(by='dribbles',ascending=False).head(10)
+print(top10_dribbles)
