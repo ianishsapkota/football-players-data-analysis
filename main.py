e0f59289f5f72bb5427be369bd6db77bb37e217a
Anish Sapkota <anishsapkota926@gmail.com>
2025-11-27 20:43:35 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+df = pd.read_csv("attacking.csv")
+
+top10_assists = df.sort_values(by='assists', ascending=False).head(10)
+print(top10_assists)
